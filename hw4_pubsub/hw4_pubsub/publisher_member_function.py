095f3307985c48e404a897310f6923de3b4eda89
jolthl <jolin_teo@mymail.sutd.edu.sg>
2024-04-01 13:00:52 +0800

    removed pid controller, changed object_callback

--- hw4_pubsub/hw4_pubsub/publisher_member_function.py
+++ hw4_pubsub/hw4_pubsub/publisher_member_function.py
@@ -14,111 +14,54 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from geomtry
+from geometry_msgs.msg import Twist
 from zed_interfaces.msg import ObjectsStamped
 
-class PID:
-    """PID (Proportional-Integral-Derivative) controller.
-
-    Used in control systems to correct the error between 
-    a measured process variable and a desired setpoint by 
-    calculating and then outputting a corrective action 
-    that can adjust the process accordingly.
-    
-    Parameters
-    ----------
-    Kp : float
-    Proportional gain
-
-    Ti : float
-    Integral time constant
-
-    Td : float 
-    Derivative time constant 
-
-    dt : float
-    Time step
-    """
-    def __init__(self, Kp, Ti, Td, dt):
-        self.Kp = Kp
-        self.Td = Td
-        self.Ti = Ti
-        self.curr_error = 0
-        self.prev_error = 0
-        self.sum_error = 0
-        self.prev_error_deriv = 0
-        self.curr_error_deriv = 0
-        self.control = 0
-        self.dt = dt
-    
-
-    def update_control(self, current_error, reset_prev=False):
-        # todo: implement this
-        # For each incoming laser scan,
-        # issue an angular velocity command to the robot at the topic /husky/cmd vel, based on the output of the PID
-        # controller. You need to follow the wall on the LEFT of the robot, as it is placed in its initial configuration.
-
-        # The PID controller should be implemented as a discrete-time controller.
-
-        if reset_prev:
-            # reset error accumulation so it doesnt grow too big in the Integral portion
-            self.sum_error = 0.0
-   
-        
-        if self.dt == 0 or self.Ti == 0:
-            # prevent division by zero in derivative calculation
-            self.control = 0.0
-            return self.control
-        
-        # update errors
-        self.prev_error_deriv = self.curr_error_deriv
-        self.prev_error = self.curr_error
-        self.curr_error = current_error
-        self.sum_error += self.curr_error
-        self.curr_error_deriv = (self.curr_error - self.prev_error) / self.dt
-
-        # PID controller formula: 
-        # Kp * error + Ki * integral(error) + Kd * derivative(error).
-        # self.Kp, self.Ti, and self.Td are the proportional, integral, and derivative gains respectively.
-        P = self.Kp * self.curr_error 
-        I = self.Kp * 1/self.Ti * self.sum_error
-        D = self.Kp * self.Td * self.curr_error_deriv
-        self.control = P + I + D
-        
-        return self.control
-      
-
-    def get_control(self):
-        return self.control
 
 class ObjectSubscriber(Node):
 
     def __init__(self):
         super().__init__('object_subscriber')
         self.zed_subscriber = self.create_subscription(String, 'zed_subscriber', 10)
-        self.pid_controller = PID(
-            Kp=self.Kp.value, 
-            Ti=self.Ti.value, 
-            Td=self.Td.value, 
-            dt=1/self.hz)
+        self.desired_distance_from_object = 0.1
+        self.forward_speed = 0.1
+
+
         # TODO: change the path as the robot is not husky
-        self.cmd_pub = self.create_publisher(Twist, '/husky/cmd_vel', QoSProfile(depth=10))
+        self.cmd_pub = self.create_publisher(Twist, '/zed/zed_node/obj_det/objects', QoSProfile(depth=10))
 
     def object_callback(self, msg):
-        """Get objects from Zed2 camera"""
+        """Get objects from Zed2 camera and move the robot"""
         objects = msg.objects
-        for obj in objects:
-            # assume object has class and position
-            obj_class = obj.object_class
-            # assume position.x: distance to the object
-            obj_distance = obj.position.x
-            self.get_logger().info('Object class: %s, distance: %f' % (obj_class, obj_distance))
-    
-    def move_callback(self):
-        """Move the robot"""
-        pass
+        # objectDistances = []
+        # for obj in objects:
+        #     # assume object has class and position
+        #     obj_class = obj.object_class
+        #     # assume position.x: distance to the object
+        #     obj_distance = obj.position.x
+        #     self.get_logger().info('Object class: %s, distance: %f' % (obj_class, obj_distance))
+        # create a Twist message
+        twist = Twist()
+        if any(obj.object_class.position.x < self.desired_distance_from_object for obj in objects):
+            twist.linear.x = 0.0
+        else:
+            twist.linear.x = self.forward_speed
+        self.cmd_pub.publish(twist)
+       
+        # # if distance > 0.1m: move forward
+        # if obj_distance > self.desired_distance_from_object:
+        #     # set the linear velocity
+        #     twist.linear.x = self.forward_speed
+        #     # publish the message
+        #     self.cmd_pub.publish(twist)
+        # else:
+        #     # stop the robot
+        #     twist.linear.x = 0.0
+        #     self.cmd_pub.publish(twist)
+      
 
 def main(args=None):
     rclpy.init(args=args)
